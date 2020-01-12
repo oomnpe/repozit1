960f821d378874907151c82af7f727cd4e4bd8e9
Vasya <mazahaka100@mail.ru>
2020-01-12 12:30:12 +0300

    module 4.1

---
+++ IntegerExperiments/src/Main.java
@@ -0,0 +1,28 @@
+import org.w3c.dom.ls.LSOutput;
+
+import static java.lang.Character.getNumericValue;
+
+public class Main {
+    public static void main(String[] args) {
+        Container container = new Container();
+        container.count += 7843;
+
+        Main Numbers = new Main();
+        Numbers.sumDigits(10071);
+        System.out.println(Numbers.allSumNumbers);
+    }
+
+    char Number;
+    int allSumNumbers;
+
+    public Integer sumDigits(Integer number) {
+
+        for (int i = 0; i < Integer.toString(number).length(); i++) {
+            Number = Integer.toString(number).charAt(i);
+            allSumNumbers = Character.digit(Number, 10) + allSumNumbers;
+            // allSumNumbers = getNumericValue(Number) + allSumNumbers;   //второй вариант
+        }
+        return allSumNumbers;
+    }
+}
+
